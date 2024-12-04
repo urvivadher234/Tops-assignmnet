e555f7222f868d2f18f358f4f1b97c5c18dbedf6
urvivadher234 <149712251+urvivadher234@users.noreply.github.com>
2024-12-04 13:37:20 +0530

    Add files via upload

---
+++ module3/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>React JS</h1>
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          <h1>Hello World</h1>
+        </p>
+      
+      </header>
+    </div>
+  );
+}
+
+export default App;
